1a96c93be33a4965e3ac6c98db5905b137eed5af
vantao28102003 <vantn.21it@vku.udn.vn>
2024-06-09 21:09:21 +0700

    nvan2903 commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,35 +17,50 @@ uploaded_file = st.sidebar.file_uploader("Select CSV", type="csv")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
-    
     # Select model type
-    model_type = st.sidebar.selectbox('Select model type', ['Logistic Regression', 'KNN', 'Random Forest', 'Decision Tree'])
-    if model_type == 'Logistic Regression':
-        model = LogisticRegression()
-    elif model_type == 'KNN':
-        k = st.sidebar.number_input("Select K", 1, len(df))
-        model = KNeighborsClassifier(n_neighbors=k)
-    elif model_type == 'Random Forest':
-        model = RandomForestClassifier(n_estimators=100)  
-    elif model_type == 'Decision Tree':
-        model = DecisionTreeClassifier()
+    model_options = ['Linear Regression', 'Logistic Regression', 'KNN', 'Random Forest', 'Decision Tree', 'Recommendation']
+    model_type = st.sidebar.selectbox('Select model type', model_options)
+    match model_type:
+        case 'Linear Regression':
+            model = LinearRegression()
+        case 'Logistic Regression':
+            model = LogisticRegression()
+        case 'KNN':
+            k = st.sidebar.number_input("Select K", 1, len(df))
+            model = KNeighborsClassifier(n_neighbors=k)
+        case 'Random Forest':
+            model = RandomForestClassifier(n_estimators=100)
+        case 'Decision Tree':
+            model = DecisionTreeClassifier()
+    target_variable = ''
+    independent_variables = ''
+    features = ''
+    visualization_var = ''
+    if model_type == 'Recommendation':
+        top_n = st.sidebar.number_input("Select top n", 5, len(df))
+        features = st.sidebar.multiselect('Select features', 
+                                                    df.columns, 
+                                                    default=list(df.columns))
+    else:
+        # Select independent and target variables
+        target_variable = st.sidebar.selectbox('Select target variable', df.columns, index=len(df.columns)-1)
+        independent_variables = st.sidebar.multiselect('Select independent variables', 
+                                                    df.columns, 
+                                                    default=list(df.columns.drop(target_variable)))
         
-    target_variable = st.sidebar.selectbox('Select target variable', df.columns, index=len(df.columns)-1)
-    independent_variables = st.sidebar.multiselect('Select independent variables', 
-                                                   df.columns, 
-                                                   default=list(df.columns.drop(target_variable)))
-    visualization_var = st.sidebar.multiselect('Select two variables for visualization', 
-                                               df.columns, 
-                                               max_selections=2)
+        # Select two variables for visualization
+        visualization_var = st.sidebar.multiselect('Select two variables for visualization', 
+                                                df.columns, 
+                                                max_selections=2)
     
     # Handle missing values
     missing_values = df.isnull().sum()
     st.sidebar.write('Missing values:', missing_values)
 
-    missing_value_options = ['Drop NA', 'Replace with Mean', 'Replace with Median', 'Replace with Mode']
+    missing_value_options = ['Drop N/A', 'Replace with Mean', 'Replace with Median', 'Replace with Mode']
     selected_option = st.sidebar.selectbox('Select option to handle missing values', missing_value_options)
 
-    if selected_option == 'Drop NA':
+    if selected_option == 'Drop N/A':
         df.dropna(inplace=True)
     elif selected_option == 'Replace with Mean':
         for col in df.columns:
@@ -58,7 +74,12 @@ if uploaded_file is not None:
         for col in df.columns:
             df[col].fillna(df[col].mode()[0], inplace=True)
 
-    X = df[independent_variables].copy()
+    if features:
+        X = df[features].copy()
+    elif independent_variables:
+        X = df[independent_variables].copy()
+    else:
+        exit()
     
     # Label encode categorical features
     le = LabelEncoder()
@@ -79,19 +100,43 @@ if st.sidebar.button('DONE'):
     df.reset_index(inplace=True, drop=True)
     st.title(model_type)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, df[target_variable], test_size=0.2, random_state=42)
-
+    # Split data to train and test
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-    model.fit(X_train, y_train)
+    if target_variable:
+        X_train, X_test, y_train, y_test = train_test_split(X, df[target_variable], test_size=0.2, random_state=42)
+
+        # Scale X data
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+        model.fit(X_train, y_train)
+    else:
+        X[features] = scaler.fit_transform(X[features])
 
     input_df = pd.DataFrame([input], columns=X.columns)
+    
+    # Label encode categorical features of user input
+    for col in X.columns:
+        if df[col].dtype == 'object':
+            le.fit_transform(df[col])
+            input_df[col] = le.transform(input_df[col])
+            
+    # Scale input data
     input_scaled = scaler.transform(input_df)
-    y_predict = model.predict(input_scaled)
-    st.subheader("Result")
-    st.write(f"{target_variable}: {y_predict[0]}")
+    
+    # Result
+    if model_type != 'Recommendation':
+        y_predict = model.predict(input_scaled)
+        st.subheader("Result")
+        st.write(f"{target_variable}: {y_predict[0]}")
+    elif model_type == 'Recommendation':
+        st.subheader('Similar rows in the dataset')
+        similarity = cosine_similarity(input_scaled, X[features].values)
+        df_copy = df.copy()
+        df_copy['similarity'] = similarity[0]
+        similar = df_copy.sort_values(by='similarity', ascending=False)[:top_n+1]
+        st.write(similar)
 
+    # Evaluation
     if model_type in ['Logistic Regression', 'KNN', 'Decision Tree', 'Random Forest']:
         accuracy = accuracy_score(y_test, model.predict(X_test)) * 100
         precision = precision_score(y_test, model.predict(X_test), average='weighted') * 100
@@ -112,7 +157,14 @@ if st.sidebar.button('DONE'):
         plt.ylabel('True labels')
         plt.title('Confusion Matrix')
         st.pyplot(plt.gcf())
+    elif model_type == 'Linear Regression':
+        st.subheader('Evaluation')
+        mse = mean_squared_error(y_test, model.predict(X_test))
+        r2 = r2_score(y_test, model.predict(X_test))
+        st.write(f"Mean Squared Error: {mse:.2f}")
+        st.write(f"R^2 Score: {r2:.2f}")
 
+    # Equation
     if model_type == 'Logistic Regression':
         coefficients = model.coef_[0]
         intercept = model.intercept_[0]
@@ -120,19 +172,30 @@ if st.sidebar.button('DONE'):
         for i, coef in enumerate(coefficients):
             equation += f"({coef:.2f} * {independent_variables[i]}) + "
         equation += f"({intercept:.2f})"
+        st.subheader('Equation')
         st.write(equation)
-        
+    elif model_type == 'Linear Regression':
+        coefficients = model.coef_
+        intercept = model.intercept_
+        equation = f"{target_variable} = "
+        for i, coef in enumerate(coefficients):
+            equation += f"({coef:.2f} * {independent_variables[i]}) + "
+        equation += f"({intercept:.2f})"
+        st.subheader('Equation')
+        st.write(equation)
+    
+    # Draw tree
     if model_type == 'Decision Tree':
         st.subheader('Decision Tree Visualization')
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 12))
-        plot_tree(model, ax=ax, feature_names=independent_variables, filled=True)
+        plot_tree(model, ax=ax, feature_names=independent_variables, filled=True, impurity=False, precision=2,  rounded=True, fontsize=12)
         st.pyplot(fig)
-        tree_text = export_text(model, feature_names=independent_variables)
+        tree_text = export_text(model, feature_names=independent_variables, decimals=3, show_weights=True)
         st.text(tree_text)
         
     if model_type == 'Random Forest':
         st.subheader('Random Forest Tree Visualization')
-        # Vẽ một cây bất kỳ từ rừng ngẫu nhiên
+        # Draw a any tree from random forest
         tree_index = st.sidebar.number_input("Select Tree Index", 0, len(model.estimators_)-1, step=1)
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 12))
         plot_tree(model.estimators_[tree_index], ax=ax, feature_names=independent_variables, filled=True)
@@ -140,6 +203,20 @@ if st.sidebar.button('DONE'):
         tree_text = export_text(model.estimators_[tree_index], feature_names=independent_variables)
         st.text(tree_text)
         
+    # Nearest neigbors
+    if model_type == 'KNN':
+        # Predict using the trained KNN model
+        y_predict = model.predict(input_scaled)
+        # Find the indices of the nearest neighbors
+        _, indices = model.kneighbors(input_scaled)
+        # Get the actual nearest neighbors from the training data
+        nearest_neighbors = X.iloc[indices[0]]
+        # Display the nearest neighbors
+        st.subheader('Nearest Neighbors')
+        st.write(nearest_neighbors)
+
+        
+    # Visualization 
     if visualization_var:
         plt.figure(figsize=(8, 6))
         plt.scatter(df[visualization_var[0]], df[visualization_var[1]], color='green')
